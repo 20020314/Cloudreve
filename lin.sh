a9f566f8a3eb4ed46eb1a8a9f0cdf3526b9f2749
20020314 <yitianyangtwo@qq.com>
2021-12-27 02:58:37 +0800

    Update and rename build.sh to lin.sh

--- lin.sh
+++ lin.sh
@@ -1,5 +1,7 @@
 apt-get update
-apt-get install wget
+apt-get install wget -y
 mkdir lin
 cd lin
 wget https://github.com/iTXTech/mcl-installer/releases/download/v1.0.3/mcl-installer-1.0.3-linux-amd64
+chmod 777 ./*
+./mcl*
